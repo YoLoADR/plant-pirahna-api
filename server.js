9cedad4297a8d72fc4d9081ceceffee31c2e5e14
Yohann Ravino <yohann.developer@gmail.com>
2024-01-27 16:54:45 +0100

    create a todo list with dynamoDB and Express.js

--- server.js
+++ server.js
@@ -22,9 +22,72 @@ app.get('/', (req, res) => {
   res.send('Bienvenue sur mon API de gestion des tasks !');
 });
 
-app.get('/todos', (req, res) => {
-    res.send('Récupère les items');
+app.get('/todos', async (req, res) => {
+    const params = {
+        TableName : 'Todos',
+    }
+    try {
+        const data = await dynamodb.scan(params).promise()
+        res.send(data.Items)
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
   });
+
+
+  app.get('/todos/:id', async (req, res) => {
+    const params = {
+        TableName : 'Todos',
+        Key:{
+            'id': req.params.id
+        }
+    }
+    try {
+        const { Item } = await dynamodb.get(params).promise()
+        if (!Item) return res.status(404).send('The todo is not found')
+        res.send(Item);
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
+  });
+
+app.put('/todos/:id', async (req, res) => {
+    const updateKeys = Object.keys(req.body);
+    if (updateKeys.length === 0) {
+        return res.status(400).send('No field to update')
+    }
+
+    const params = {
+        TableName: 'Todos',
+        Key: {
+            'id' : req.params.id
+        },
+        UpdateExpression: "set",
+        ExpressionAttributeNames: {},
+        ExpressionAttributeValues: {}
+    };
+
+
+    updateKeys.forEach((key, index) => {
+        const attributeKey = `#attr${key}`;
+        const attributeValue = `:value${key}`;
+        params.UpdateExpression += ` ${attributeKey} = ${attributeValue}`;
+        params.ExpressionAttributeNames[attributeKey] = key;
+        params.ExpressionAttributeValues[attributeValue] = req.body[key];
+
+        if (index < updateKeys.length - 1) {
+            params.UpdateExpression += ",";
+          }
+    });
+
+    try {
+        const data = await dynamodb.update(params).promise();
+        res.send(data.Attributes);
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
+
+});
 
 app.post('/todos', async (req, res) => {
     const todo = {
@@ -44,6 +107,22 @@ app.post('/todos', async (req, res) => {
         res.status(500).send('Aucun champ à mettre à jour .')
     }
 });
+
+app.delete('/todos/:id', async (req, res)=>{
+    const params = {
+        TableName: 'Todos',
+        Key:{
+            'id': req.params.id
+        }
+    };
+
+    try {
+        await dynamodb.delete(params).promise();
+        res.send({ id: req.params.id })
+    } catch {
+        res.status(500).send(err.toString());
+    }
+})
 
 
 
